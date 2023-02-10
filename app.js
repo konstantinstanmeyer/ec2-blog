f54ac721e9e9d42241c5d67537624bd5d3fbb346
KonstantinStanmeyer <konstantin@stanmeyer.com>
2023-02-10 11:47:36 -0700

    update

--- app.js
+++ app.js
@@ -4,4 +4,4 @@ const app = express();
 
 app.get('/', (req, res) => res.json({ message: "App works!" }))
 
-app.listen(3000, () => console.log("Server is running!"));
+app.listen(3001, () => console.log("Server is running!"));
