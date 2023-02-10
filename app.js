ad941ad877cc7e983c5312c5eb7401abf40b97fd
KonstantinStanmeyer <konstantin@stanmeyer.com>
2023-02-10 11:19:47 -0700

    setup

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => res.json({ message: "App works!" }))
+
+app.listen(3000, () => console.log("Server is running!"));
